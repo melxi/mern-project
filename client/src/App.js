6dd5c5930f02d38ced44ceb6e919660149278580
Melxi <magomed.futijev@gmail.com>
2020-01-05 19:16:15 +0200

    created react client app and AppNavbar component styling it with reactstrap

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import AppNavbar from './components/AppNavbar'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <AppNavbar />
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default App
---
+++ client/src/components/AppNavbar.js
@@ -0,0 +1,45 @@
+import React, {useState} from 'react'
+
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  Container
+} from 'reactstrap'
+
+const AppNavbar = () => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const toggle = () => {
+    console.log(isOpen)
+    setIsOpen(!isOpen)
+  }
+
+  return (
+    <div>
+      <Navbar className='mb-5' color='dark' dark expand='sm'>
+        <Container>
+          <NavbarBrand href='/'>
+            ShoppingList
+          </NavbarBrand>
+          <NavbarToggler onClick={toggle}></NavbarToggler>
+          <Collapse isOpen={isOpen} navbar>
+            <Nav className='ml-auto' navbar>
+              <NavItem>
+                <NavLink href='https://github.com/melxi'>
+                  Github
+                </NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  )
+}
+
+export default AppNavbar
