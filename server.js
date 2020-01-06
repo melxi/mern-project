7688caeae64b50af8e947c0c915142cec0c4e005
Melxi <magomed.futijev@gmail.com>
2020-01-06 21:10:22 +0200

    version 1.0.0

--- server.js
+++ server.js
@@ -19,14 +19,13 @@ mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology
 app.use('/api/items', itemsRouter)
 
 // Serve static assets if in production
-if (config.NODE_ENV == 'production') {
-  // Set static folder
-  app.use(express.static('client/build'))
+// Set static folder
+app.use(express.static('client/build'))
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  })
-}
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+})
+
 
 // Connect to PORT
 app.listen(config.PORT, () => console.log(`Server running on port ${config.PORT}`))
--- utils/config.js
+++ utils/config.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 let MONGODB_URI = process.env.MONGODB_URI;
 let PORT = process.env.PORT || 3003
+
 module.exports = {
   MONGODB_URI,
   PORT
